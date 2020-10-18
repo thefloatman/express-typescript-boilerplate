008a2a7c4aa3ea335379ea39704ead5dbd46b4a1
whitequbits <bagus.gitdata@gmail.com>
2020-10-18 12:42:32 +0700

    Add model test

---
+++ tests/models/user.model.test.ts
@@ -0,0 +1,33 @@
+import { getRepository } from "typeorm";
+import connection from "../../src/utils/connection";
+import { User } from "../../src/models/user.model";
+
+describe("User Model", () => {
+  const user = new User();
+  user.firstName = "Timber";
+  user.lastName = "Saw";
+  user.email = "timber@gmail.com";
+  user.password = "123456789";
+
+  beforeAll(async () => {
+    await connection.create();
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  beforeEach(async () => {
+    await connection.clear();
+  });
+
+  test("it should create user", async () => {
+
+    await getRepository(User).save(user);
+
+    const loadedUser = await getRepository(User).findOne(user);
+
+    expect(loadedUser).toBeDefined();
+  });
+});
+
